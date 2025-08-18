8f9b203325551273387a7883c5ba84a5537d4940
ashwajitdua <ashwajitdua@gmail.com>
2025-08-18 23:19:15 +0800

    Updated code to add character_counter dictionary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_num_words
 
+from stats import word_counter
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -9,6 +11,9 @@ def main():
     text = get_book_text('books/frankenstein.txt')
     word_count = get_num_words(text)
     print(f"{word_count} words found in the document")
+    num_of_characters = word_counter(text)
+    print (num_of_characters)
 
 if __name__ == "__main__":
     main()
+
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def get_num_words(text):
     words = text.split()
     count= len(words) 
     return count
+
+def word_counter(text):
+    character_counter = {}
+    for character in text:
+        character = character.lower()
+        if character in character_counter:
+            character_counter [character] += 1
+        else:
+            character_counter [character] = 1
+    return character_counter
+        
