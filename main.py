25e2cc8ab1606c9f7138f64ae886874dc197c1ab
ashwajitdua <ashwajitdua@gmail.com>
2025-08-20 02:09:37 +0800

    updated py files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from stats import get_num_words
 
 from stats import word_counter
 
+from stats import sorted_list
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -10,9 +12,19 @@ def get_book_text(filepath):
 def main():
     text = get_book_text('books/frankenstein.txt')
     word_count = get_num_words(text)
-    print(f"{word_count} words found in the document")
     num_of_characters = word_counter(text)
-    print (num_of_characters)
+    my_sorted_char_list = sorted_list(num_of_characters)
+    print ("============ BOOKBOT ============ ")
+    print ("Analyzing book found at books/frankenstein.txt...") 
+    print ("----------- Word Count ---------- ")
+    print (f"Found {word_count} total words")
+    print ("--------- Character Count -------")
+    for num_dictionary in my_sorted_char_list:
+        char = num_dictionary["char"]
+        count = num_dictionary["num"]
+        if char.isalpha() == True:
+            print (f"{char}: {count}")    
+    print ("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -12,4 +12,13 @@ def word_counter(text):
         else:
             character_counter [character] = 1
     return character_counter
-        
+
+def sorted_list(character_dictionary):
+    Sorted_dictionaries = []
+    for char, char_count in character_dictionary.items():
+        num_dictionary = {"char": char, "num": char_count}
+        Sorted_dictionaries.append(num_dictionary)
+    Sorted_dictionaries.sort(reverse=True, key= lambda char: char["num"])
+    return Sorted_dictionaries
+
+
